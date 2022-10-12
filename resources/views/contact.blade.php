3fac2a4ead8c5bfc28c6ebc02303c413db0006df
FarhanHossain00 <farhanhossenbd@gmail.com>
2022-10-12 21:48:58 +0600

    v2

--- resources/views/contact.blade.php
+++
@@ -1,5 +0,0 @@
-@extends("layout.default")
-@section("title","contact")
-@section('intro')
-<h1>contact page override intro</h1>
-@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,1 @@
-@extends("layout.default")
-@section("title","homepage")
-@section('intro')
-@parent
-<h1 class="red">HOmepage adding into section</h1>
-@endsection
-
-@section('homebanner')
-    <h1>this is home banner section</h1>
-@endsection
+<h1>let's make something incredible</h1>
